ad6da408cadf6a7bb747d4759b17b6e4f747040e
Gabriel Becker <83714713+Gabriel-Becker@users.noreply.github.com>
2024-11-03 02:10:30 -0300

    Criado basico da tela de home de Caixa e Admin

---
+++ frontend-EasyPark/src/pages/AdminHome/AdminHome.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function AdminHome() {
+  return (
+    <div>
+      <h1>Bem-vindo, Administrador!</h1>
+      {/* Conteúdo específico para administradores */}
+    </div>
+  );
+} 
---
+++ frontend-EasyPark/src/pages/CaixaHome/CaixaHome.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function CaixaHome() {
+  return (
+    <div>
+      <h1>Bem-vindo, Caixa!</h1>
+      {/* Conteúdo específico para caixas */}
+    </div>
+  );
+} 
