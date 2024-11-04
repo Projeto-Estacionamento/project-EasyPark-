0968063f4438241c3adf1c1b3dddbba9736576f7
Gabriel Becker <83714713+Gabriel-Becker@users.noreply.github.com>
2024-11-04 09:49:22 -0300

    Componente de input

--- frontend-EasyPark/src/components/input/InputField.jsx
+++ frontend-EasyPark/src/components/input/InputField.jsx
@@ -1,1 +1,16 @@
- 
+import React from 'react';
+import './InputField.css';
+
+export function InputField({ label, type, name, value, onChange }) {
+  return (
+    <div className="input-field">
+      <label>{label}</label>
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+} 
