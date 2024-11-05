84ea4e4aa65737c718cb90408efd2aa1a5ba8701
Gabriel Becker <83714713+Gabriel-Becker@users.noreply.github.com>
2024-11-05 00:23:45 -0300

    Update Button

--- frontend-EasyPark/src/components/button/button.jsx
+++ frontend-EasyPark/src/components/button/button.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import "./button.css"
+import './Button.css';
 
-export function Button({type, onClick, children, color, size}) {
-
-    const buttonStyle = {
-        backgroundColor: color,
-        border: 'none',
-        padding: '10px 20px',
-        color: 'white',
-        cursor: 'pointer',
-        borderRadius: '5px',
-        fontSize: '16px',
-        width: size
-    };
-
-    return (
-        <button style={buttonStyle} type={type} onClick={onClick} className="botao">{children}</button>
-    )
+export function Button({ 
+  children, 
+  type = 'button', 
+  onClick, 
+  variant = 'primary',
+  fullWidth = false 
+}) {
+  return (
+    <button 
+      type={type} 
+      onClick={onClick}
+      className={`button ${variant} ${fullWidth ? 'full-width' : ''}`}
+    >
+      {children}
+    </button>
+  );
 }
