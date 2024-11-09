aa75f3b71afdd85956a5996832ebea8fc802d8f7
Gabriel Becker <83714713+Gabriel-Becker@users.noreply.github.com>
2024-11-09 19:58:25 -0300

    Criado CaixaHome

--- frontend-EasyPark/src/pages/CaixaHome/CaixaHome.jsx
+++ frontend-EasyPark/src/pages/CaixaHome/CaixaHome.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from "../../components/button/Button";
+import './CaixaPage.css';
 
-export function CaixaHome() {
+export function CaixaPage() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Bem-vindo, Caixa!</h1>
-      {/* Conteúdo específico para caixas */}
+    <div className="home-page">
+      <h1>Bem-vindo ao EasyPark!</h1>
+      <div className="menu">
+        <Button onClick={() => navigate('/entrada-estacionamento')}>
+          Entrada no Estacionamento
+        </Button>
+        <Button onClick={() => navigate('/relatorio-usuarios')}>
+          Relatório de Usuários
+        </Button>
+      </div>
     </div>
   );
 } 
