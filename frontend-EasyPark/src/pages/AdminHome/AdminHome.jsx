39c4fdc3dc9d23afd0a302588128bbd66f28e8e5
Gabriel Becker <83714713+Gabriel-Becker@users.noreply.github.com>
2024-11-17 19:19:46 -0300

    Update AdminHome

--- frontend-EasyPark/src/pages/AdminHome/AdminHome.jsx
+++ frontend-EasyPark/src/pages/AdminHome/AdminHome.jsx
@@ -1,46 +1,26 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button } from "../../components/button/Button";
+import { PageContainer } from '../../components/pageContainer/PageContainer';
+import { Card } from '../../components/card/Card';
+import { Button } from '../../components/button/Button';
 import './AdminHome.css';
 
 export function AdminHome() {
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    // Lógica para logout
-    navigate('/');
-  };
-
   return (
-    <div className="admin-home">
-      <h1>Bem-vindo, Administrador!</h1>
-      <div className="menu">
-        <Button onClick={() => navigate('/configuracao-estacionamento')}>
-          Configuração de Estacionamento
-        </Button>
-        <Button onClick={() => navigate('/configuracao-acesso')}>
-          Configuração de Usuários
-        </Button>
-        <Button onClick={() => navigate('/relatorio-usuarios')}>
-          Relatório de Usuários
-        </Button>
-        <Button onClick={() => navigate('/gerenciamento-cliente')}>
-          Gerenciamento de Cliente
-        </Button>
-        <Button onClick={() => navigate('/gerenciamento-plano')}>
-          Gerenciamento de Plano
-        </Button>
-        <Button onClick={() => navigate('/gerenciamento-assinatura')}>
-          Gerenciamento de Assinatura
-        </Button>
-        <Button onClick={() => navigate('/tela-ticket')}>
-          Tela do Ticket
-        </Button>
-      </div>
-      <Button className="logout-button" onClick={handleLogout}>
-        Logout
-      </Button>
-    </div>
+    <PageContainer darkMode>
+      <Card title="Admin Home">
+        <div className="menu">
+          <Button onClick={() => navigate('/configuracao-estacionamento')}>Configuração de Estacionamento</Button>
+          <Button onClick={() => navigate('/gerenciamento-cliente')}>Gerenciamento de Cliente</Button>
+          <Button onClick={() => navigate('/gerenciamento-plano')}>Gerenciamento de Plano</Button>
+          <Button onClick={() => navigate('/gerenciamento-assinatura')}>Gerenciamento de Assinatura</Button>
+          <Button onClick={() => navigate('/relatorio')}>Relatório</Button>
+          <Button onClick={() => navigate('/configuracao-acesso')}>Configuração de Acesso</Button>
+        </div>
+      </Card>
+    </PageContainer>
   );
 }
 export default AdminHome;  
