d330bdc87a192db1414dc2b1ae709bf21c7c1c6f
Gabriel Becker <83714713+Gabriel-Becker@users.noreply.github.com>
2024-11-17 19:19:36 -0300

    Update Card

--- frontend-EasyPark/src/components/card/Card.jsx
+++ frontend-EasyPark/src/components/card/Card.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Card.css';
 
-export function Card({ children, title }) {
+export function Card({ title, children }) {
   return (
     <div className="card">
-      {title && <h1 className="card-title">{title}</h1>}
-      {children}
+      {title && <h2 className="card-title">{title}</h2>}
+      <div className="card-content">
+        {children}
+      </div>
     </div>
   );
 } 
