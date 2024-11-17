f34f3ab4a9badf3d17c843f8383f024fe395cd81
Gabriel Becker <83714713+Gabriel-Becker@users.noreply.github.com>
2024-11-17 19:19:56 -0300

    Update  CaixaHome

--- frontend-EasyPark/src/pages/CaixaHome/CaixaHome.jsx
+++ frontend-EasyPark/src/pages/CaixaHome/CaixaHome.jsx
@@ -1,44 +1,26 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button } from "../../components/button/Button";
+import { PageContainer } from '../../components/pageContainer/PageContainer';
+import { Card } from '../../components/card/Card';
+import { Button } from '../../components/button/Button';
 import './CaixaHome.css';
 
 export function CaixaHome() {
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    // Lógica para logout
-    navigate('/');
-  };
-
   return (
-    <div className="caixa-home">
-      <h1>Bem-vindo ao EasyPark!</h1>
-      <div className="menu">
-        <Button onClick={() => navigate('/configuracao-estacionamento')}>
-          Configuração de Estacionamento (Visualização)
-        </Button>
-        <Button onClick={() => navigate('/relatorio-usuarios')}>
-          Relatório de Usuários
-        </Button>
-        <Button onClick={() => navigate('/gerenciamento-cliente')}>
-          Gerenciamento de Cliente
-        </Button>
-        <Button onClick={() => navigate('/gerenciamento-plano')}>
-          Gerenciamento de Plano
-        </Button>
-        <Button onClick={() => navigate('/gerenciamento-assinatura')}>
-          Gerenciamento de Assinatura
-        </Button>
-        <Button onClick={() => navigate('/tela-ticket')}>
-          Tela do Ticket
-        </Button>
-      </div>
-      <Button className="logout-button" onClick={handleLogout}>
-        Logout
-      </Button>
-    </div>
+    <PageContainer darkMode>
+      <Card title="Caixa Home">
+        <div className="menu">
+          <Button onClick={() => navigate('/configuracao-estacionamento')}>Configuração de Estacionamento (Visualização)</Button>
+          <Button onClick={() => navigate('/gerenciamento-cliente')}>Gerenciamento de Cliente</Button>
+          <Button onClick={() => navigate('/gerenciamento-plano')}>Gerenciamento de Plano</Button>
+          <Button onClick={() => navigate('/gerenciamento-assinatura')}>Gerenciamento de Assinatura</Button>
+          <Button onClick={() => navigate('/relatorio')}>Relatório</Button>
+        </div>
+      </Card>
+    </PageContainer>
   );
-} 
+}
 
 export default CaixaHome;  
