307dea0e4395120250ccae12f3a77f951108d663
Gabriel Becker <83714713+Gabriel-Becker@users.noreply.github.com>
2024-11-17 22:42:54 -0300

    Update homes

--- frontend-EasyPark/src/pages/CaixaHome/CaixaHome.jsx
+++ frontend-EasyPark/src/pages/CaixaHome/CaixaHome.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { PageContainer } from '../../components/pageContainer/PageContainer';
+import { BackgroundWrapper } from '../../components/backgroundWrapper/BackgroundWrapper';
 import { Card } from '../../components/card/Card';
 import { Button } from '../../components/button/Button';
 import './CaixaHome.css';
@@ -9,17 +9,17 @@ export function CaixaHome() {
   const navigate = useNavigate();
 
   return (
-    <PageContainer darkMode>
+    <BackgroundWrapper>
       <Card title="Caixa Home">
         <div className="menu">
-          <Button onClick={() => navigate('/configuracao-estacionamento')}>Configuração de Estacionamento (Visualização)</Button>
+          <Button onClick={() => navigate('/configuracao-estacionamento-caixa')}>Configuração de Estacionamento (Visualização)</Button>
           <Button onClick={() => navigate('/gerenciamento-cliente')}>Gerenciamento de Cliente</Button>
           <Button onClick={() => navigate('/gerenciamento-plano')}>Gerenciamento de Plano</Button>
           <Button onClick={() => navigate('/gerenciamento-assinatura')}>Gerenciamento de Assinatura</Button>
           <Button onClick={() => navigate('/relatorio')}>Relatório</Button>
         </div>
       </Card>
-    </PageContainer>
+    </BackgroundWrapper>
   );
 }
 
