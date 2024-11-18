4c50522c07a66370ee0e4a2de889ee44bd4317c1
Gabriel Becker <83714713+Gabriel-Becker@users.noreply.github.com>
2024-11-17 22:42:09 -0300

    Componente backgroundWrapper

---
+++ frontend-EasyPark/src/components/backgroundWrapper/BackgroundWrapper.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './BackgroundWrapper.css';
+
+export function BackgroundWrapper({ children }) {
+  return <div className="background-wrapper">{children}</div>;
+} 
