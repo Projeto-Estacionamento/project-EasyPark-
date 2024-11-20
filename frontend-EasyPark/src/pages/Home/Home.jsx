38949f00ac740519ff8bb518c3d993fb2253832e
Gabriel Becker <83714713+Gabriel-Becker@users.noreply.github.com>
2024-11-20 18:00:32 -0300

    Adicionado imagem de fundo ao home

--- frontend-EasyPark/src/pages/Home/Home.jsx
+++ frontend-EasyPark/src/pages/Home/Home.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { SidebarMenu } from '../../components/sidebarMenu/SidebarMenu';
 import { Button } from '../../components/button/button';
+import { Card } from '../../components/card/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Home.css';
 
 export function Home() {
   return (
-    <div className="d-flex">
+    <div className="d-flex home-container">
       <SidebarMenu />
-      <div className="flex-grow-1 d-flex flex-column justify-content-center align-items-center p-3">
-        <Button className="mb-3" style={{ width: '200px' }}>
-          Entrada Estacionamento
-        </Button>
-        <Button style={{ width: '200px' }}>
-          Saída Estacionamento
-        </Button>
+      <div className="flex-grow-1 d-flex flex-column justify-content-center align-items-center">
+        <div className="background-image"></div>
+        <Card>
+          <div className="d-flex flex-column justify-content-around align-items-center" style={{ height: '300px', width: '400px' }}>
+            <Button variant="outline-light" style={{ width: '100%' }}>
+              Entrada
+            </Button>
+            <Button variant="outline-light" style={{ width: '100%' }}>
+              Saída
+            </Button>
+          </div>
+        </Card>
       </div>
     </div>
   );
