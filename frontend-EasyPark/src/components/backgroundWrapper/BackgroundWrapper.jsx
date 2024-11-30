10ffc7edd3e124af6133ebdf48d8a071cf808fe7
Gabriel Becker <83714713+Gabriel-Becker@users.noreply.github.com>
2024-11-29 22:07:14 -0300

    merge

--- frontend-EasyPark/src/components/backgroundWrapper/BackgroundWrapper.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-export function BackgroundWrapper({ children }) {
-  return <div className="d-flex justify-content-center align-items-center min-vh-100">{children}</div>;
-}
--- frontend-EasyPark/src/components/container/Container.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-export function Container({ children }) {
-  return <div className="container d-flex flex-column justify-content-center align-items-center min-vh-100">{children}</div>;
-} 
