a5ae217eb9a840ade16a7aac5cbb84d3da678a5f
Gabriel Becker <83714713+Gabriel-Becker@users.noreply.github.com>
2024-12-04 10:21:19 -0300

    Update ConfiguracaoEstacionamentoAdmin.jsx
    Toast aplicado

--- frontend-EasyPark/src/pages/ConfiguracaoEstacionamento/ConfiguracaoEstacionamentoAdmin.jsx
+++ frontend-EasyPark/src/pages/ConfiguracaoEstacionamento/ConfiguracaoEstacionamentoAdmin.jsx
@@ -3,6 +3,8 @@ import { ConfiguracaoForm } from '../../components/configuracao/ConfiguracaoForm
 import { getConfiguracaoAtual, updateConfiguracao } from '../../services/ConfiguracaoSistemaService';
 import { SidebarMenu } from '../../components/sidebarMenu/SidebarMenu';
 import { Button } from '../../components/button/button';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function ConfiguracaoEstacionamentoAdmin() {
   const [configuracao, setConfiguracao] = useState({
@@ -16,7 +18,6 @@ export function ConfiguracaoEstacionamentoAdmin() {
     valorDiariaMoto: 0.0,
     horaMaximaAvulso: 0.0,
   });
-
 
   useEffect(() => {
     const fetchConfiguracao = async () => {
@@ -40,51 +41,51 @@ export function ConfiguracaoEstacionamentoAdmin() {
     e.preventDefault();
     try {
       await updateConfiguracao(configuracao);
-      alert('Configuração atualizada com sucesso!');
+      toast.success('Configuração atualizada com sucesso!');
     } catch (error) {
       console.error('Erro ao atualizar configuração:', error);
+      toast.error('Erro ao atualizar configuração!');
     }
   };
 
-  const tipoUsuario = sessionStorage.getItem('accessType');
-
-    return (
-        <div className="d-flex">
-            <SidebarMenu />
-            <div className="configuracao-container">
-            <h2 className="header-title">Configuração do Estacionamento</h2>
-            <form onSubmit={handleSubmit}>
-                    <div className="form-group">
-                        <label>Quantidade de Vagas (Moto)</label>
-                        <input type="number" name="qtdMoto" value={configuracao.qtdMoto} onChange={handleChange} />
-                    </div>
-                    <div className="form-group">
-                        <label>Quantidade de Vagas (Carro)</label>
-                        <input type="number" name="qtdCarro" value={configuracao.qtdCarro} onChange={handleChange} />
-                    </div>
-                    <div className="form-group">
-                        <label>Valor Hora (Moto)</label>
-                        <input type="number" name="valorHoraMoto" value={configuracao.valorHoraMoto} onChange={handleChange} />
-                    </div>
-                    <div className="form-group">
-                        <label>Valor Hora (Carro)</label>
-                        <input type="number" name="valorHoraCarro" value={configuracao.valorHoraCarro} onChange={handleChange} />
-                    </div>
-                    <div className="form-group">
-                        <label>Valor Diário (Moto)</label>
-                        <input type="number" name="valorDiariaMoto" value={configuracao.valorDiariaMoto} onChange={handleChange} />
-                    </div>
-                    <div className="form-group">
-                        <label>Valor Diário (Carro)</label>
-                        <input type="number" name="valorDiariaCarro" value={configuracao.valorDiariaCarro} onChange={handleChange} />
-                    </div>
-                    <div className="form-group">
-                        <label>Hora Máxima Avulso</label>
-                        <input type="number" name="horaMaximaAvulso" value={configuracao.horaMaximaAvulso} onChange={handleChange} />
-                    </div>
-                    <Button type="submit" fullWidth>Salvar</Button>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="d-flex">
+      <SidebarMenu />
+      <div className="configuracao-container">
+        <h2 className="header-title">Configuração do Estacionamento</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Quantidade de Vagas (Moto)</label>
+            <input type="number" name="qtdMoto" value={configuracao.qtdMoto} onChange={handleChange} />
+          </div>
+          <div className="form-group">
+            <label>Quantidade de Vagas (Carro)</label>
+            <input type="number" name="qtdCarro" value={configuracao.qtdCarro} onChange={handleChange} />
+          </div>
+          <div className="form-group">
+            <label>Valor Hora (Moto)</label>
+            <input type="number" name="valorHoraMoto" value={configuracao.valorHoraMoto} onChange={handleChange} />
+          </div>
+          <div className="form-group">
+            <label>Valor Hora (Carro)</label>
+            <input type="number" name="valorHoraCarro" value={configuracao.valorHoraCarro} onChange={handleChange} />
+          </div>
+          <div className="form-group">
+            <label>Valor Diário (Moto)</label>
+            <input type="number" name="valorDiariaMoto" value={configuracao.valorDiariaMoto} onChange={handleChange} />
+          </div>
+          <div className="form-group">
+            <label>Valor Diário (Carro)</label>
+            <input type="number" name="valorDiariaCarro" value={configuracao.valorDiariaCarro} onChange={handleChange} />
+          </div>
+          <div className="form-group">
+            <label>Hora Máxima Avulso</label>
+            <input type="number" name="horaMaximaAvulso" value={configuracao.horaMaximaAvulso} onChange={handleChange} />
+          </div>
+          <Button type="submit" fullWidth>Salvar</Button>
+        </form>
+        <ToastContainer />
+      </div>
+    </div>
+  );
 }
